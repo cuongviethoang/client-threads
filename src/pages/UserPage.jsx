ad92e8f6b093e2c0170053497d7d1459e79e6e00
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-03-17 10:42:27 +0700

    hoàn thành trang post và trong page của người dùng

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,32 @@
 import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
 const UserPage = () => {
     return (
         <>
             <UserHeader />
+            <UserPost
+                likes={1200}
+                replies={481}
+                postImg="/post1.png"
+                postTitle="Let's talk about threads"
+            />
+            <UserPost
+                likes={1000}
+                replies={500}
+                postImg="/post2.png"
+                postTitle="Nice tutorial"
+            />
+            <UserPost
+                likes={900}
+                replies={200}
+                postImg="/post3.png"
+                postTitle="I love this guy."
+            />
+            <UserPost
+                likes={400}
+                replies={200}
+                postTitle="This is my first threads"
+            />
         </>
     );
 };
