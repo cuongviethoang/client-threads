da232280e798736fa0e7ba0cfa2d59333928b121
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-03-20 09:32:31 +0700

    gọi api xử lý like và unlike

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,6 @@ import { formatDistanceToNow } from "date-fns";
 
 const Post = ({ post, postedBy }) => {
     const navigation = useNavigate();
-    const [liked, setLiked] = useState(false);
     const showToast = useShowToast();
     const [user, setUser] = useState(null);
 
@@ -143,22 +142,7 @@ const Post = ({ post, postedBy }) => {
                     )}
 
                     <Flex gap={3}>
-                        <Actions liked={liked} setLiked={setLiked} />
-                    </Flex>
-                    <Flex gap={2} alignItems={"center"}>
-                        <Text color={"gray.light"} fontSize={"sm"}>
-                            {post?.replies.length} replies
-                        </Text>
-                        <Box
-                            w={0.5}
-                            h={0.5}
-                            borderRadius={"full"}
-                            bg={"gray.light"}
-                        ></Box>
-
-                        <Text color={"gray.light"} fontSize={"sm"}>
-                            {post?.likes.length} likes
-                        </Text>
+                        <Actions post={post} />
                     </Flex>
                 </Flex>
             </Flex>
